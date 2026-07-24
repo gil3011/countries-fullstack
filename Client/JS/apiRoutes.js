0400356b65800ab3d9a13463de1408df6714e8e5
RonOmer <ronomer4@gmail.com>
2026-07-24 15:56:34 +0300

    another fix

--- Client/JS/apiRoutes.js
+++ Client/JS/apiRoutes.js
@@ -1,7 +1,7 @@
 ﻿const develop_mode = true; //change when developing/publishing
 
-const ruppin_path = "https://proj.ruppin.ac.il/cgroup24/test2/tar1";
-const local_path = "https://localhost:7255"
+const ruppin_path = "https://proj.ruppin.ac.il/cgroup24/test2/tar1/api";
+const local_path = "https://localhost:7255/api"
 
 const BASE_API = develop_mode ? local_path : ruppin_path;
 
